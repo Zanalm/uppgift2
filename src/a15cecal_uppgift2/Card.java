2fbc902323da045321f925d37e0b513a6930f083
Zander <31848787+Zanalm@users.noreply.github.com>
2018-01-17 17:45:21 +0100

    indentering
    indenteringsfix

--- src/a15cecal_uppgift2/Card.java
+++ src/a15cecal_uppgift2/Card.java
@@ -2,32 +2,27 @@ package a15cecal_uppgift2;
 
 public class Card {
 	private String cardType;
-	private int cardNumber; 
-	
-	//constructor
-	public Card(String type, int number)
-	{
+	private int cardNumber;
+
+	// constructor
+	public Card(String type, int number) {
 		cardType = type;
 		cardNumber = number;
 	}
-	
-	public String getType()
-	{
+
+	public String getType() {
 		return cardType;
 	}
-	
-	public int getNumber()
-	{
+
+	public int getNumber() {
 		return cardNumber;
 	}
-	
-	//Used to return object with its variables in 
-	//ArrayList rather than its memory address
+
+	// Used to return object with its variables in
+	// ArrayList rather than its memory address
 	@Override
-	public String toString()
-	{
-		return (cardNumber + " of "
-				+ cardType);
-				
+	public String toString() {
+		return (cardNumber + " of " + cardType);
+
 	}
 }
